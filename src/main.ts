6801db2d10fa995eb5a4ad55166a5e95b585bde9
RayanEreteo <rayabf5@gmail.com>
2026-01-31 20:46:52 +0100

    show result container on fetch

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 // HTML elements ref
-const form = document.getElementById("dry-form")
+const form = document.getElementById("dry-form") as HTMLFormElement
 const input = document.getElementById("city-input") as HTMLInputElement
 const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement
 
-form?.addEventListener("submit", (e) => {
+const resultContainer = document.getElementById("result-container") as HTMLElement
+
+form.addEventListener("submit", (e) => {
   submitBtn.disabled = true
   e.preventDefault()
   fetchWeather(input.value)
 })
 
 async function fetchWeather(city:string) {
+  resultContainer.style.display = "block"
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=20370396598f014dfec9a4efab56a08f`)
     const data = response.json()
